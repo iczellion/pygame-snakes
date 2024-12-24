9fbe02b4934c15ba968a3e9ff41838194a75a34d
Eric Beaudet <galpha@gmail.com>
2024-12-24 10:45:31 -0500

    Remove unused import

--- src/ai_controller.py
+++ src/ai_controller.py
@@ -1,7 +1,7 @@
 import os
 
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.callbacks import CheckpointCallback
 import pygame
 
